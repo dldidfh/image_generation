77ca89004147c865c6736a9ca3b8c2892c8e6548
yangro <dldidfh2@gmail.com>
2021-07-23 17:04:12 +0900

    2021_07_23_flip_rotation_imgaug

--- rotate2.py
+++ rotate2.py
@@ -20,21 +20,19 @@ def box_value_resize(center_x, center_y, box_width, box_height, resize_pactor):
 
 image_path = './test2.jpg'
 gt_path = './test2.txt'
-origin_rotate_gt_save_path = './origin_rotation_result.txt'
-flip_rotate_gt_save_path = './flip_rotation_result.txt'
+save_path = ['./origin_rotation', './flip_rotation']
 
 image = cv2.imread('./test2.jpg', cv2.IMREAD_COLOR)
 h, w = image.shape[:2]
 
 resize_pactor = (300,300)
-resize_h, resize_w = image.shape[:2]
 image = cv2.resize(image,resize_pactor)
+resize_h, resize_w = image.shape[:2]
 
-
-name_space = ['origin', 'flip']
 
 origin_ground_truth_list = []
-flip_ground_truth_list = []
+
+image_batch_size = (4,3)
 
 with open(gt_path) as read:
     boxes = read.readlines()
@@ -50,52 +48,56 @@ with open(gt_path) as read:
         ymin = re_center_y - re_box_height/2
         xmax = re_center_x + re_box_width/2
         ymax = re_center_y + re_box_height/2
-        flip_xmax = flip_center_x + re_box_width/2
-        flip_xmin = flip_center_x - re_box_width/2
-    
+
         origin_ground_truth_list.append([class_num, xmin, ymin, xmax, ymax])
-        flip_ground_truth_list.append([class_num,flip_xmin, ymin,flip_xmax,ymax])
-temp_list = [origin_ground_truth_list,flip_ground_truth_list]
-origin_imgaug_bounding_boxes = []
-flip_imgaug_bounding_boxes = []
-for count,temp in enumerate(temp_list):
+
+temp_list = [origin_ground_truth_list,origin_ground_truth_list]
+
+# temp list에는 각각 원본의 
+for count in range(2):
     imgaug_bounding_boxes = []
     # if count ==0:
-    for box in temp:
+    for box in origin_ground_truth_list:
         imgaug_bounding_boxes.append(ia.BoundingBox(x1=box[1],y1=box[2],x2=box[3],y2=box[4],label=box[0]))
     bbs = ia.BoundingBoxesOnImage(imgaug_bounding_boxes,shape=image.shape)
-    if count == 0:
-        seq = iaa.Sequential([
-            iaa.Affine(rotate=20),
-        ])
-    elif count==1:
-        seq = iaa.Sequential([
-            iaa.Fliplr(1.0),
-            iaa.Affine(rotate=20),
-        ])
-    image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
-    if count==0:
-        cv2.imwrite('./origin_rotate.jpg',image_aug)
-        with open('./origin_rotate.txt', 'w') as wd:
-            for one_box in bbs_aug:
-                rotate_box_width = one_box.width / resize_w
-                rotate_box_height = one_box.height / resize_h
-                rotate_center_x = one_box.center_x / resize_w
-                rotate_center_y = one_box.center_y / resize_h
-                string = "{} {} {} {} {}\n".format(one_box.label, rotate_center_x, rotate_center_y, rotate_box_width, rotate_box_height)
-                wd.write(string)
-    elif count==1 : 
-        cv2.imwrite('./flip_rotate.jpg',image_aug)
-        with open('./flip_rotate.txt', 'w') as wd:
-            for one_box in bbs_aug:
-                rotate_box_width = one_box.width / resize_w
-                rotate_box_height = one_box.height / resize_h
-                rotate_center_x = one_box.center_x / resize_w
-                rotate_center_y = one_box.center_y / resize_h
-                string = "{} {} {} {} {}\n".format(one_box.label, rotate_center_x, rotate_center_y, rotate_box_width, rotate_box_height)
-                wd.write(string)
-
-
-    # elif count == 1:
-    #     for box in temp:
-    #         flip_imgaug_bounding_boxes.append(ia.BoundingBox(x1=box[1],y1=box[2],x2=box[3],y2=box[4],label=box[0]))
+    index = 1
+    result_box = []
+    for num_1 in range(image_batch_size[1]):
+        three_images = np.zeros(image.shape)
+        for num_2 in range(image_batch_size[0]):
+            degree = index * 7.5
+            index += 1  
+            if count == 0:
+                seq = iaa.Sequential([
+                    iaa.Affine(rotate=degree),
+                ])
+            elif count==1:
+                seq = iaa.Sequential([
+                    iaa.Fliplr(1.0),
+                    iaa.Affine(rotate=degree),
+                ])
+            image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
+            # 바운딩박스 좌표값 수정해서 새로운 배열에 추가 
+            for box_1 in bbs_aug:
+                rotate_box_width = round(box_1.width / (image_batch_size[0] * resize_w),6)
+                rotate_box_height = round(box_1.height / (image_batch_size[1] * resize_h), 6)
+                rotate_center_x = round(((box_1.center_x + resize_w*num_2) / (image_batch_size[0] * resize_w)) , 6)
+                rotate_center_y = round(((box_1.center_y + resize_h*num_1) / (image_batch_size[1] * resize_h)) ,6)
+                result_box.append([
+                    box_1.label, rotate_center_x, rotate_center_y, rotate_box_width, rotate_box_height
+                ])
+            
+            if num_2 == 0:
+                three_images = image_aug
+            else:
+                three_images = np.concatenate((three_images,image_aug), axis=1)
+        if num_1 == 0:
+            twelve_images = three_images
+        else:
+            twelve_images = np.concatenate((twelve_images,three_images), axis=0)
+    cv2.imwrite(save_path[count] + '.jpg' ,twelve_images)
+    with open(save_path[count] + '.txt', 'w') as wd:
+        for one_box in result_box:            
+            string = "{} {} {} {} {}\n".format(one_box[0], one_box[1], one_box[2], one_box[3], one_box[4])
+            wd.write(string)
+ 
